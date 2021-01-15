0f0305dea917bf19624a8ef39269db174a6dad96
Andrey Bashkirtcev <nruzumak@gmail.com>
2021-01-15 19:08:21 +0500

    init

---
+++ cocoapodtest/Main.swift
@@ -0,0 +1,14 @@
+//
+//  Main.swift
+//  cocoapodtest
+//
+//  Created by Andrey Baskirtcev on 15.01.2021.
+//
+
+import Foundation
+
+public class Main {
+    public func testing() -> String {
+        return "Allright!"
+    }
+}
---
+++ cocoapodtest/cocoapodtest.h
@@ -0,0 +1,18 @@
+//
+//  cocoapodtest.h
+//  cocoapodtest
+//
+//  Created by Andrey Baskirtcev on 15.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cocoapodtest.
+FOUNDATION_EXPORT double cocoapodtestVersionNumber;
+
+//! Project version string for cocoapodtest.
+FOUNDATION_EXPORT const unsigned char cocoapodtestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocoapodtest/PublicHeader.h>
+
+
